12c500d2daa8f9d73d80039e77a034006dfa6f66
ochenudivlen <70331127+ochenudivlen@users.noreply.github.com>
2021-09-18 01:24:13 +0300

    Add files via upload

---
+++ GenerateString.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void GenerateString (int n, char* string);
+
+int main()
+{
+    int number;
+    scanf ("%d", &number);
+
+    char* s;
+
+    GenerateString (number, s);
+
+    return 0;
+}
+
+void GenerateString (int n, char* string)
+{
+    int val = 1; 
+    char* more_string;
+    char* temp_string;
+
+    strcpy (more_string, "a"); 
+
+    printf ("%s\n", more_string);
+
+    temp_string = more_string;
+    more_string = more_string + 1;
+    val = (val * 2) + 1;
+ 
+    for (int i = 1; i < n; i++)
+    {
+        string = (char*)calloc(val, sizeof (char));
+
+        strcpy (string, temp_string);
+        strcat (string, more_string);
+        strcat (string, temp_string);
+
+        more_string = more_string + 1;
+        temp_string = string;
+        val = (val * 2) + 1;
+
+        printf ("%s\n", string);
+    }
+}
