adc214e376c3ed8b2dbe8a20ced193ee6ed920ce
Русаков Андрей <70331127+ochenudivlen@users.noreply.github.com>
2021-10-03 22:32:37 +0300

    Add files via upload

--- GenerateString.c
+++ GenerateString.c
@@ -9,7 +9,7 @@ int main()
     int number;
     scanf ("%d", &number);
 
-    char* s;
+    char* s = NULL;
 
     GenerateString (number, s);
 
@@ -18,35 +18,32 @@ int main()
 
 void GenerateString (int n, char* string)
 {
-    int   val = 1; 
-    char  more_string;
-    char* temp_string;
-    temp_string = (char*)calloc(1, sizeof (char));
-
-    more_string = 'a'; 
+    int  val = 1; 
+    char more_string = 'a';
 
     printf ("%c\n", more_string);
 
-    temp_string[0] = more_string;
-    more_string = more_string + 1;
-    val = (val * 2) + 1;
+    for (int i = 0; i < (n - 1); i++)
+        val = (val * 2) + 1;
+
+    string = (char*)calloc((val * 2) + 1, sizeof (char));
+    char* temp_string = (char*)calloc(val, sizeof (char));
  
+    temp_string[0] = more_string;
+    more_string++;
+
     for (int i = 1; i < n; i++)
     {
-        string = (char*)calloc(val, sizeof (char));
-
         strcpy (string, temp_string);
         string [strlen(string)] = more_string;
         strcat (string, temp_string);
 
-        more_string = more_string + 1;
-        temp_string = string;
-        val = (val * 2) + 1;
+        strcpy (temp_string, string);
+        more_string++;
 
         printf ("%s\n", string);
     }
 
     free (string);
+    free (temp_string);
 }
-
-// TODO: вы позвали calloc n+1 раз, а free только 1 раз
